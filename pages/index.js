59584987153843f2a55c112e4ebd3e6ddc7e28e1
Wendell Gabriel Fernandes Barros <102330689+mrwendellbarros@users.noreply.github.com>
2022-11-07 17:40:51 -0300

    baixando framework

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function HomePage() {
+  const estiloDaHomePage = { backgoundcolor: "red"};
+  return (
+    <div style={estiloDaHomePage} > Bem vindo ao AluraTube!</div>
+  );
+}
+
+export default HomePage
