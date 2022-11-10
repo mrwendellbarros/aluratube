f62105d5d0958e5b5b94e802e06ab3c47e2475bc
Wendell Gabriel Fernandes Barros <102330689+mrwendellbarros@users.noreply.github.com>
2022-11-10 17:16:39 -0300

    construindo header

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,64 @@
+import config from "../config.json";
+import styled from "styled-components";
+
 function HomePage() {
-  const estiloDaHomePage = { backgoundcolor: "red"};
+  const estiloDaHomePage = { backgroundColor: "red" };
   return (
-    <div style={estiloDaHomePage} > Bem vindo ao AluraTube!</div>
+    <div style={estiloDaHomePage} >
+      <Menu />
+      <Header />
+      <Timeline />
+    </div>
   );
 }
 
 export default HomePage
+
+function Menu() {
+  return (
+    <div>
+      Menu
+    </div>
+  )
+}
+
+const StyledHeader = styled.div `
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+  .user-info {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+  }
+`;
+function Header() {
+  return (
+    <StyledHeader>
+     {/* <img src="banner" /> */}
+     <section className="user-info">
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+            <h2>
+            {config.name}
+            </h2>
+            <p>
+            {config.job}
+            </p>
+        </div>
+     </section>
+    </StyledHeader>
+  )
+}
+
+function Timeline() {
+  return (
+    <div>
+     Timeline
+    </div>
+  )
+}
