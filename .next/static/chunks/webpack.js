1b7f57772e731f462387ec6a93d0839e6d8dddfa
Wendell Gabriel Fernandes Barros <102330689+mrwendellbarros@users.noreply.github.com>
2022-11-10 22:59:01 -0300

    att layout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "35c33416619d3ffe"; }
+/******/ 		__webpack_require__.h = function() { return "7686f877a4ba74c9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,90 @@
-import config from "../config.json";
-import styled from "styled-components";
+import config from '../config.json'
+import styled from 'styled-components'
+import { CSSReset } from '../src/components/CSSReset'
+import Menu from '../src/components/Menu'
+import { StyledTimeline } from '../src/components/Timeline'
 
 function HomePage() {
-  const estiloDaHomePage = { backgroundColor: "red" };
+  const estiloDaHomePage = { 
+    //backgroundColor: 'red' 
+  }
+
+  //console.log(config.playlists);
+
   return (
-    <div style={estiloDaHomePage} >
-      <Menu />
-      <Header />
-      <Timeline />
-    </div>
-  );
+    <>
+      <CSSReset />
+      <div style={estiloDaHomePage}>
+        <Menu />
+        <Header />
+        <Timeline playlists={config.playlists} />
+      </div>
+    </>
+  )
 }
 
 export default HomePage
 
-function Menu() {
-  return (
-    <div>
-      Menu
-    </div>
-  )
-}
+/* function Menu() {
+  return <div>Menu</div>
+} */
 
-const StyledHeader = styled.div `
+const StyledHeader = styled.div`
   img {
     width: 80px;
     height: 80px;
     border-radius: 50%;
   }
   .user-info {
+    margin-top: 50px;
     display: flex;
     align-items: center;
     width: 100%;
     padding: 16px 32px;
     gap: 16px;
   }
-`;
+`
 function Header() {
   return (
     <StyledHeader>
-     {/* <img src="banner" /> */}
-     <section className="user-info">
+      {/* <img src="banner" /> */}
+      <section className="user-info">
         <img src={`https://github.com/${config.github}.png`} />
         <div>
-            <h2>
-            {config.name}
-            </h2>
-            <p>
-            {config.job}
-            </p>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
         </div>
-     </section>
+      </section>
     </StyledHeader>
   )
 }
 
-function Timeline() {
+function Timeline(propidades) {
+  //console.log("Dentro do componente", propidades.playlists);
+  const playListName = Object.keys(propidades.playlists)
+
   return (
-    <div>
-     Timeline
-    </div>
+    <StyledTimeline>
+      {playListName.map(playListName => {
+        const vídeos = propidades.playlists[playListName]
+        console.log(playListName)
+        console.log(vídeos)
+        return (
+          <section>
+            <h2>{playListName}</h2>
+            <div>
+              {vídeos.map(vídeos => {
+                return (
+                  <a href={vídeos.url}>
+                    <img src={vídeos.thumb} />
+                    <span>{vídeos.title}</span>
+                  </a>
+                )
+              })}
+            </div>
+          </section>
+        )
+      })}
+    </StyledTimeline>
   )
 }
